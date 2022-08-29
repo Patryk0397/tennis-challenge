57c99a8320e24622a0ffdd54adfcb40b57b43342
Patryk Orsztynowicz <patrykorsztynowicz1997@gmail.com>
2022-08-29 21:14:58 +0100

    Created a Player class

---
+++ Player.js
@@ -0,0 +1,16 @@
+// Player Class
+export default class Player {
+  constructor(name) {
+    this.name = name;
+    this.gameScore = 0;
+    this.setScore = 0;
+  }
+
+  addPoint = () => {
+    this.gameScore++;
+  };
+
+  addSetPoint = () => {
+    this.setScore++;
+  };
+}

9fb00ea2d931248751182a583df2f5db23d686b9
Patryk Orsztynowicz <patrykorsztynowicz1997@gmail.com>
2022-08-30 09:13:26 +0100

    Corrected variable names to be more fitting to purpose

--- Player.js
+++ Player.js
@@ -2,19 +2,19 @@
 export default class Player {
   constructor(name) {
     this.name = name;
+    this.score = 0;
     this.gameScore = 0;
-    this.setScore = 0;
   }
 
   addPoint = () => {
+    this.score++;
+  };
+
+  addGamePoint = () => {
     this.gameScore++;
   };
 
-  addSetPoint = () => {
-    this.setScore++;
-  };
-
   resetScore = () => {
-    this.gameScore = 0;
+    this.score = 0;
   };
 }
